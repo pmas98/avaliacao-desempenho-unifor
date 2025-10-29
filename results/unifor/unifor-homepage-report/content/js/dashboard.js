0508d7198708cd61cc15790408f508af355deb6c
Pedro Fernandes <pmas98@hotmail.com>
2025-10-29 13:52:12 -0300

    Update navigation results and summary files with new page data for Post-Graduation and Admissions. Remove outdated accessibility report files and clean up project structure by deleting unused resources, including CSS, JS, and font files from the sbadmin2 directory.

--- results/unifor/unifor-homepage-report/content/js/dashboard.js
+++ results/unifor/unifor-homepage-report/content/js/dashboard.js
@@ -173,7 +173,7 @@ $(document).ready(function() {
     });
 
     // Creates APDEX table
-    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.942, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [0.942, 500, 1500, "Homepage Load"], "isController": false}]}, function(index, item){
+    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.994, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [0.994, 500, 1500, "Homepage Load"], "isController": false}]}, function(index, item){
         switch(index){
             case 0:
                 item = item.toFixed(3);
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 500, 0, 0.0, 399.8239999999997, 342, 987, 364.0, 635.9000000000001, 656.95, 715.7900000000002, 6.041346978722376, 267.0386043959861, 2.1475100588427196], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Homepage Load", 500, 0, 0.0, 399.8239999999997, 342, 987, 364.0, 635.9000000000001, 656.95, 715.7900000000002, 6.041346978722376, 267.0386043959861, 2.1475100588427196], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 500, 0, 0.0, 275.4159999999997, 254, 719, 265.0, 289.0, 310.0, 517.8600000000001, 6.207170523388618, 274.33202358802384, 2.206455146985798], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["Homepage Load", 500, 0, 0.0, 275.4159999999997, 254, 719, 265.0, 289.0, 310.0, 517.8600000000001, 6.207170523388618, 274.33202358802384, 2.206455146985798], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
